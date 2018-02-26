e30daab1448ac97dafc44b0f0c3fa35b00a9119a
EC2 Default User <ec2-user@ip-172-31-65-49.ec2.internal>
2018-02-26 04:55:53 +0000

    updated to only allow local ssh

--- helloworld-cf-template.py
+++ helloworld-cf-template.py
@@ -11,7 +11,13 @@ from troposphere import (
     Template,
 )
 
+from ipaddress import ip_network
+
+from ipify import get_ip
+
 ApplicationPort = "3000"
+
+PublicCidrIp = str(ip_network(get_ip()))
 
 t = Template()
 
@@ -32,7 +38,7 @@ t.add_resource(ec2.SecurityGroup(
             IpProtocol="tcp",
             FromPort="22",
             ToPort="22",
-            CidrIp="0.0.0.0/0",
+            CidrIp=PublicCidrIp,
         ),
         ec2.SecurityGroupRule(
             IpProtocol="tcp",
